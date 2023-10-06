cc909f9d53a5bc3c44a1f203931f8bd51d887bfd
JustCris02 <cristianecom02@gmail.com>
2023-10-05 23:51:05 -0400

    added functionality to Exercise List

---
+++ frontend/src/components/ExerciseList.jsx
@@ -0,0 +1,10 @@
+export default function ExerciseList(){
+
+
+    return(
+        <div className="ExerciseList">
+            <h1>Swap Exercise</h1>
+            <input type="search"></input>
+        </div>
+    )
+}
--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import ExerciseList from "./ExerciseList"
 export default function Menu(props){
+
+    const [showExerciseList,setShowExerciseList]=useState(false);
+
     return(
         <div className ="optionMenu">
-            <p className="optionItem">Swap Exercise</p>
-            <p className="optionItem">Remove Exercise</p>
-            <p className="optionItem" onClick={props.removeSet}>Remove Set</p>
-            <p className="optionItem">Change Weight Unit (lbs)</p>
+            { showExerciseList && <ExerciseList /> }
+            {!showExerciseList&&<p className="optionItem" onClick={()=>setShowExerciseList(prev=>!prev)} >Swap Exercise</p>}
+            {!showExerciseList&& <p className="optionItem">Remove Exercise</p>}
+            {!showExerciseList&&<p className="optionItem" onClick={props.removeSet}>Remove Set</p>}
+            {!showExerciseList&&<p className="optionItem">Change Weight Unit (lbs)</p>}
         </div>
     )
 }
